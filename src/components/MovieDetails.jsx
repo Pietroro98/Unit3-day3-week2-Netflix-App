8b78e35f2928b44b9c328cb2d7355915728f0478
Pietroro98 <pietroro98@gmail.com>
2024-08-28 17:23:25 +0200

    modifiche

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Button, Spinner, Alert } from 'react-bootstrap';
-import { useParams } from 'react-router-dom'; // Assicurati di avere react-router-dom installato
+import { useParams } from 'react-router-dom'; 
 
 const MovieDetails = () => {
-  const { movieId } = useParams(); // Estrai l'id del film dalla URL
+  const { movieId } = useParams(); 
   const [movieDetails, setMovieDetails] = useState(null);
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,10 +11,10 @@ const MovieDetails = () => {
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
-      const apiKey = "662fb97c"; // La tua chiave API
+      const apiKey = "662fb97c"; 
 
       try {
-        // Fetch per i dettagli del film
+       
         const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`);
         const movieData = await movieResponse.json();
         if (movieData.Response === 'True') {
@@ -23,10 +23,10 @@ const MovieDetails = () => {
           setIsError(true);
         }
 
-        // Fetch per i commenti del film
+        
         const commentsResponse = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieId}`, {
           headers: {
-            'Authorization': 'Bearer YOUR_ACCESS_TOKEN', // Sostituisci con il tuo token di autenticazione
+            'Authorization': 'Bearer YOUR_ACCESS_TOKEN', 
           },
         });
         const commentsData = await commentsResponse.json();
